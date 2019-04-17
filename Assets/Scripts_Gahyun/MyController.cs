faa7f1f40199838e0415318bc8545a0a638169e0
gamulchi <32320763+gamulchi@users.noreply.github.com>
2019-04-18 00:30:36 +0900

    컨트롤러 수정

---
+++ Assets/Scripts_Gahyun/MyController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using valve.vr 임포트 해야함...
+
+public class MyController : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
